a9c0a54ab37b8c085bf29379c54b1de642dcf0d3
Kewei Zhang <kwzhang@Keweis-MBP.fios-router.home>
2020-03-21 16:50:10 -0400

    temp try

--- src/Main.py
+++ src/Main.py
@@ -1,6 +1,27 @@
 from flask import Flask
+from flask import request, render_template
 
 app = Flask(__name__)
 
+
+@app.route('/hello') #what url will execute the function
+def helloWorld():
+    return ("Hello world")
+
+
+@app.route('/') 
+@app.route('/<name>')
+def index():
+    return render_template("index.html")
+
+"""
+@app.route('/')
+def hello():
+    name = request.args.get("name", "World")
+    return f'Hello, {escape(name)}!'
+
+"""
 if __name__ == "__main__":
+    app.debug = True
     app.run()
+
