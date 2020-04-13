abd9b4f8a1f2a03acfdc23932f8f641802ce3a14
Kewei Zhang <jeffzhkw@gmail.com>
2020-04-13 18:32:53 -0400

    delete redundant file

--- src/recording.py
+++
@@ -1,8 +0,0 @@
-class Recording(object):
-    '''
-    Storing the recording itself
-    '''
-    def __init__(self, name, length, data):
-        self.name = name
-        self.length = length
-        self.data = data
